2435917e4afdccfcc1d828f2ba96da2f2ae6bee2
chriswait <chriswait91@gmail.com>
2022-08-07 12:52:14 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+
+(async () => {
+  const db = await open({
+    filename: "database.db",
+    driver: sqlite3.Database,
+  });
+  await db.migrate();
+
+  const response = await fetch(
+    "https://portal.rockgympro.com/portal/public/620b59568a6c93407373bda88564f747/occupancy"
+  );
+  const body = await response.text();
+  const count = body.match(/'count' : (?<count>\d+),/).groups.count;
+  const capacity = body.match(/'capacity' : (?<capacity>\d+),/).groups.capacity;
+  db.exec(
+    `insert into Logs values ("${new Date().toISOString()}", ${count}, ${capacity})`
+  );
+})();
